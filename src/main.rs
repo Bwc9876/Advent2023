11f0aad1637ca71f971e575b1d6337ecdb928ebd
Ben C <bwc9876@gmail.com>
2023-12-03 02:08:21 -0500

    Add run all option

--- src/main.rs
+++ src/main.rs
@@ -30,20 +30,59 @@ fn match_part(part: u32, day: Box<dyn Day>, input: &str) -> i32 {
     }
 }
 
+fn run_day(day_num: u32, part: u32, input: &str) {
+    let day = match_day(day_num);
+
+    let start = std::time::Instant::now();
+
+    let result = match_part(part, day, input);
+
+    println!(
+        "Day {} Part {} Result: {} (in {}ms)",
+        day_num,
+        part,
+        result,
+        start.elapsed().as_millis()
+    );
+}
+
+fn run_all_days() {
+    let start = std::time::Instant::now();
+
+    for day_num in 1..=3 {
+        for part in 1..=2 {
+            let day = match_day(day_num);
+
+            let input = day.get_input();
+
+            run_day(day_num, part, input);
+        }
+    }
+
+    println!("Total time: {}ms", start.elapsed().as_millis());
+}
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
     let (date, part) = if let Some(arg) = args.get(1) {
-        if arg == "help" {
-            println!("Usage: advent_2023 [day:part] [input]");
-            std::process::exit(0);
-        } else {
-            let date_part = arg.split(':').collect::<Vec<&str>>();
+        match arg.trim() {
+            "help" => {
+                println!("Usage: advent_2023 [day:part] [input]");
+                std::process::exit(0);
+            }
+            "*" => {
+                run_all_days();
+                std::process::exit(0);
+            }
+            _ => {
+                let date_part = arg.split(':').collect::<Vec<&str>>();
 
-            let date = date_part.first().unwrap().parse::<u32>().unwrap();
-            let part = date_part.get(1).unwrap().parse::<u32>().unwrap();
+                let date = date_part.first().unwrap().parse::<u32>().unwrap();
+                let part = date_part.get(1).unwrap().parse::<u32>().unwrap();
 
-            (date, part)
+                (date, part)
+            }
         }
     } else {
         select()
@@ -71,11 +110,7 @@ fn main() {
         day.get_input().to_string()
     };
 
-    let start = std::time::Instant::now();
+    let _start = std::time::Instant::now();
 
-    let result = match_part(part, day, &input);
-
-    println!("Day {} Part {} Result: {}", date, part, result);
-
-    println!("Done in {}ms", start.elapsed().as_millis());
+    run_day(date, part, &input);
 }
