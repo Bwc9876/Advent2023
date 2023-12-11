730bcd85df595ea0f6db66dade43ff46ad9d5423
Ben C <bwc9876@gmail.com>
2023-12-10 19:44:35 -0500

    Day 10 organization

--- src/day_10.rs
+++ src/day_10.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{day::Day, get_input_for_day};
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     North,
     South,
@@ -19,9 +19,28 @@ impl Direction {
             Self::West => Self::East
         }
     }
+
+    pub fn ninety_deg(&self, clockwise: bool) -> Self {
+        match self {
+            Self::North => if clockwise { Self::East } else { Self::West },
+            Self::South => if clockwise { Self::West } else { Self::East },
+            Self::East => if clockwise { Self::South } else { Self::North },
+            Self::West => if clockwise { Self::North } else { Self::South }
+        }
+    }
+
+    pub fn add_to_pos(&self, pos: (usize, usize)) -> (i32, i32) {
+        let pos = (pos.0 as i32, pos.1 as i32);
+        match self {
+            Self::North => (pos.0, pos.1 - 1),
+            Self::South => (pos.0, pos.1 + 1),
+            Self::East => (pos.0 + 1, pos.1),
+            Self::West => (pos.0 - 1, pos.1)
+        }
+    }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy)]
 enum MapTile {
     Vertical,
     Horizontal,
@@ -49,31 +68,14 @@ impl MapTile {
     }
 
     pub fn determine_from_directions(a: &Direction, b: &Direction) -> Self {
-        match a {
-            Direction::North => match b {
-                Direction::South => Self::Vertical,
-                Direction::East => Self::NeBend,
-                Direction::West => Self::NwBend,
-                _ => panic!("Invalid directions")
-            },
-            Direction::South => match b {
-                Direction::North => Self::Vertical,
-                Direction::East => Self::SeBend,
-                Direction::West => Self::SwBend,
-                _ => panic!("Invalid directions")
-            },
-            Direction::East => match b {
-                Direction::North => Self::NeBend,
-                Direction::South => Self::SeBend,
-                Direction::West => Self::Horizontal,
-                _ => panic!("Invalid directions")
-            },
-            Direction::West => match b {
-                Direction::North => Self::NwBend,
-                Direction::South => Self::SwBend,
-                Direction::East => Self::Horizontal,
-                _ => panic!("Invalid directions")
-            }
+        match (a, b) {
+            (Direction::North, Direction::South) | (Direction::South, Direction::North) => Self::Vertical,
+            (Direction::East, Direction::West) | (Direction::West, Direction::East) => Self::Horizontal,
+            (Direction::North, Direction::East) | (Direction::East, Direction::North) => Self::NeBend,
+            (Direction::North, Direction::West) | (Direction::West, Direction::North) => Self::NwBend,
+            (Direction::South, Direction::East) | (Direction::East, Direction::South) => Self::SeBend,
+            (Direction::South, Direction::West) | (Direction::West, Direction::South) => Self::SwBend,
+            _ => panic!("Can't determine tile from directions")
         }
     }
 
@@ -92,37 +94,12 @@ impl MapTile {
 
     pub fn follow(&self, coming_from: Direction) -> Direction {
         match self {
-            Self::Vertical => match coming_from {
-                Direction::North => Direction::South,
-                Direction::South => Direction::North,
-                _ => panic!("Can't follow vertical tile from non-vertical direction")
-            },
-            Self::Horizontal => match coming_from {
-                Direction::East => Direction::West,
-                Direction::West => Direction::East,
-                _ => panic!("Can't follow horizontal tile from non-horizontal direction")
-            },
-            Self::NeBend => match coming_from {
-                Direction::North => Direction::East,
-                Direction::East => Direction::North,
-                _ => panic!("Can't follow NE bend tile from non-NE direction")
-            },
-            Self::NwBend => match coming_from {
-                Direction::North => Direction::West,
-                Direction::West => Direction::North,
-                _ => panic!("Can't follow NW bend tile from non-NW direction")
-            },
-            Self::SeBend => match coming_from {
-                Direction::East => Direction::South,
-                Direction::South => Direction::East,
-                _ => panic!("Can't follow SE bend tile from non-SE direction")
-            },
-            Self::SwBend => match coming_from {
-                Direction::South => Direction::West,
-                Direction::West => Direction::South,
-                _ => panic!("Can't follow SW bend tile from non-SW direction")
-            },
-            Self::Start => coming_from,
+            Self::Vertical | Self::Horizontal => coming_from.opposite(),
+            Self::NeBend => coming_from.ninety_deg(matches!(coming_from, Direction::North)),
+            Self::NwBend => coming_from.ninety_deg(matches!(coming_from, Direction::West)),
+            Self::SeBend => coming_from.ninety_deg(matches!(coming_from, Direction::East)),
+            Self::SwBend => coming_from.ninety_deg(matches!(coming_from, Direction::South)),
+            Self::Start => panic!("Can't follow start tile as it's unknown"),
             Self::Empty => panic!("Can't follow empty tile")
         }
     }
@@ -132,23 +109,43 @@ fn find_start(map: &[Vec<MapTile>]) -> (usize, usize) {
     map.iter().enumerate().find_map(|(y, row)| row.iter().enumerate().find_map(|(x, tile)| if matches!(*tile, MapTile::Start) { Some((x, y)) } else { None })).unwrap()
 }
 
-fn get_adjacent(pos: (usize, usize), map: &Vec<Vec<MapTile>>) -> Vec<((usize, usize), Direction)> {
-    let mut adjacent = vec![];
+fn get_adjacents(pos: (usize, usize), map: &Vec<Vec<MapTile>>) -> Vec<((usize, usize), Direction)> {
+    vec![Direction::North, Direction::South, Direction::East, Direction::West].into_iter().filter_map(|dir| {
+        let pos = dir.add_to_pos(pos);
 
-    if pos.1 > 0 {
-        adjacent.push(((pos.0, pos.1 - 1), Direction::North));
-    }
-    if pos.0 < map[0].len() - 1 {
-        adjacent.push(((pos.0 + 1, pos.1), Direction::East));
-    }
-    if pos.1 < map.len() - 1 {
-        adjacent.push(((pos.0, pos.1 + 1), Direction::South));
-    }
-    if pos.0 > 0 {
-        adjacent.push(((pos.0 - 1, pos.1), Direction::West));
+        if pos.0 > 0 && pos.1 > 0 && pos.0 < map[0].len() as i32 && pos.1 < map.len() as i32 {
+            Some(((pos.0 as usize, pos.1 as usize), dir))
+        } else {
+            None
+        }
+    }).collect()
+}
+
+fn get_connectors_of_tile(pos: (usize, usize), map: &Vec<Vec<MapTile>>) -> Vec<((usize, usize), Direction)> {
+    let adjacents = get_adjacents(pos, map);
+    adjacents.into_iter().filter(|adjacent| map[adjacent.0.1][adjacent.0.0].has_direction(&adjacent.1.opposite())).collect::<Vec<_>>()
+}
+
+fn follow_loop(map: &Vec<Vec<MapTile>>) -> HashMap<(usize, usize), MapTile> {
+    
+    let start_pos = find_start(map);
+    let connectors = get_connectors_of_tile(start_pos, map);
+    let start_tile = MapTile::determine_from_directions(&connectors[0].1, &connectors[1].1);
+
+    let mut current = connectors[0];
+    let tile = map[current.0.1][current.0.0];
+
+    let mut loop_tiles = [(start_pos, start_tile), (current.0, tile)].iter().copied().collect::<HashMap<_, _>>();
+
+    while current.0 != start_pos {
+        let current_tile = &map[current.0.1][current.0.0];
+        let direction = current_tile.follow(current.1.opposite());
+        let next_pos = direction.add_to_pos(current.0);
+        loop_tiles.insert(current.0, *current_tile);
+        current = ((next_pos.0 as usize, next_pos.1 as usize), direction);
     }
 
-    adjacent
+    loop_tiles
 }
 
 pub struct Day10;
@@ -160,89 +157,29 @@ impl Day for Day10 {
     fn part_1(&self, input: &str) -> i64 {
         let map = input.lines().map(|line| line.chars().map(MapTile::parse).collect::<Vec<_>>()).collect::<Vec<_>>();
 
-        let start_pos = find_start(&map);
+        let tiles = follow_loop(&map);
 
-        let adjacent_to_start = get_adjacent(start_pos, &map);
-
-        let connectors = adjacent_to_start.into_iter().filter(|(pos, dir)| map.get(pos.1).and_then(|r| r.get(pos.0)).map(|t| t.has_direction(&dir.opposite())).unwrap_or(false)).collect::<Vec<_>>();
-
-        let mut count = 1;
-        let mut current = connectors.into_iter().next().unwrap();
-
-        while current.0 != start_pos {
-            let current_tile = &map[current.0.1][current.0.0];
-
-            let direction = current_tile.follow(current.1.opposite());
-
-            let next_pos = (current.0.0 as i32 + match direction {
-                Direction::East => 1,
-                Direction::West => -1,
-                _ => 0
-            }, current.0.1 as i32 + match direction {
-                Direction::South => 1,
-                Direction::North => -1,
-                _ => 0
-            });
-
-            current = ((next_pos.0 as usize, next_pos.1 as usize), direction);
-            count += 1;
-        }
-
-        count / 2
+        (tiles.len() / 2) as i64
     }
 
     fn part_2(&self, input: &str) -> i64 {
         let map = input.lines().map(|line| line.chars().map(MapTile::parse).collect::<Vec<_>>()).collect::<Vec<_>>();
 
-        let start_pos = find_start(&map);
+        let loop_tiles = follow_loop(&map);
 
-        let adjacent_to_start = get_adjacent(start_pos, &map);
-
-        let connectors = adjacent_to_start.into_iter().filter(|(pos, dir)| map.get(pos.1).and_then(|r| r.get(pos.0)).map(|t| t.has_direction(&dir.opposite())).unwrap_or(false)).collect::<Vec<_>>();
-
-        let start_tile = MapTile::determine_from_directions(&connectors[0].1, &connectors[1].1);
-
-        let mut current = connectors.into_iter().next().unwrap();
-
-        let tile = map[current.0.1][current.0.0];
-
-        let mut loop_tiles = [(start_pos, start_tile), (current.0, tile)].iter().cloned().collect::<HashMap<_, _>>();
-
-        while current.0 != start_pos {
-
-            let current_tile = &map[current.0.1][current.0.0];
-
-            let direction = current_tile.follow(current.1.opposite());
-
-            let next_pos = (current.0.0 as i32 + match direction {
-                Direction::East => 1,
-                Direction::West => -1,
-                _ => 0
-            }, current.0.1 as i32 + match direction {
-                Direction::South => 1,
-                Direction::North => -1,
-                _ => 0
-            });
-
-            loop_tiles.insert(current.0, *current_tile);
-
-            current = ((next_pos.0 as usize, next_pos.1 as usize), direction);
-        }
-
-        let mut count = 0;
-
-        for (y, row) in map.iter().enumerate() {
-            let mut toggle = false;
-            for (x, _) in row.iter().enumerate() {
+        let count = map.iter().enumerate().fold(0, |count, (y, row)| {
+            let row_count = row.iter().enumerate().fold((0, false), |(count, toggle), (x, _)| {
                 if let Some(tile) = loop_tiles.get(&(x, y)) {
                     if tile.has_direction(&Direction::South) {
-                        toggle = !toggle;
+                        return (count, !toggle);
                     }
                 } else if toggle {
-                    count += 1;
+                    return (count + 1, toggle);
                 }
-            }
-        }
+                (count, toggle)
+            }).0;
+            count + row_count
+        });
 
        count
     }
@@ -256,13 +193,13 @@ mod tests {
     fn test_part_1() {
         let day = Day10;
         let input = day.get_input();
-        assert_eq!(day.part_1(&input), 6815);
+        assert_eq!(day.part_1(input), 6815);
     }
 
     #[test]
     fn test_part_2() {
         let day = Day10;
         let input = day.get_input();
-        assert_eq!(day.part_2(&input), 269);
+        assert_eq!(day.part_2(input), 269);
     }
 }
